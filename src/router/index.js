7d29cb98660f8250cea0b30ffafdf28b0215ce82
sharonLin <sharonlin5963@gmail.com>
2021-11-21 20:24:50 +0800

    完成首頁畫面

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,6 @@ const routes = [
     name: 'Home',
     component: Home
   }
-  // {
-  //   path: '/scenicSpot',
-  //   name: 'ScenicSpot',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (scenicSpot.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "scenicSpot" */ '../views/ScenicSpot.vue')
-  // }
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <div class="home-backend-img">
+    <img src="../assets/images/backend/index_bg.svg" alt="index background">
+    </div>
     <div>
       <div class="subLogo">
         <img src="../assets/images/logo/subLogo.svg" alt="logo_BIKE全書">
@@ -9,34 +12,31 @@
           <img src="../assets/images/logo/BikeBOOK_logo.svg" alt="logo_BIKE全書">
         </div>
         <div class="tabs">
-          <div>
-            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-              <path fill="#E6E6E6" :d="icons.mdiMapMarkerRadius" />
+          <div class="tab tab_active">
+            <svg viewBox="0 0 24 24">
+              <path :d="icons.mdiMapMarkerRadius" />
             </svg>
             <h4>YouBike站點</h4>
           </div>
-          <div>
-            <svg class="svg_mdiBikeFast" style="width:24px;height:24px" viewBox="0 0 24 24">
-              <path fill="#E6E6E6" :d="icons.mdiBikeFast" />
+          <div class="tab">
+            <svg class="svg_mdiBikeFast" viewBox="0 0 24 24">
+              <path :d="icons.mdiBikeFast" />
             </svg>
             <h4>自行車道路線</h4>
           </div>
         </div>
         <div class="radios">
-          <h3>搜尋條件</h3>
+          <h3 class="title">搜尋條件</h3>
           <div>
             <el-radio v-model="radio1" label="1">該縣市全部</el-radio>
             <el-radio v-model="radio1" label="2">目前位置附近</el-radio>
           </div>
         </div>
         <div class="select">
-          <h3>目的地</h3>
-          <el-button round>
-            請選擇前往的縣/市
-            <svg class="svg_mdiBikeFast" style="width:24px;height:24px" viewBox="0 0 24 24">
-              <path fill="#0000000C" :d="icons.mdiChevronDown" />
-            </svg>
-          </el-button>
+          <h3 class="title">目的地</h3>
+          <el-select v-model="destination" placeholder="請選擇前往的縣/市">
+            <el-option></el-option>
+          </el-select>
         </div>
         <el-button type="primary" round>OPEN BOOK</el-button>
       </div>
@@ -52,6 +52,11 @@ export default {
   name: 'Home',
   components: {
   },
+  data () {
+    return {
+      destination: ''
+    }
+  },
   setup () {
     const icons = { mdiMapMarkerRadius, mdiBikeFast, mdiChevronDown }
     return {
@@ -61,8 +66,3 @@ export default {
   }
 }
 </script>
-<style lang="scss">
-.svg_mdiBikeFast{
-  transform: scale(1,-1) rotate(180deg);
-}
-</style>
